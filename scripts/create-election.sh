e3c5d8023f31772ce32460978fba764d58ae823a
Steven Landers <steven.landers@gmail.com>
2018-04-25 14:27:26 -0400

    adding pin checking and updating address

---
+++ scripts/create-election.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl -X POST -d "isPublic=true&metadataLocation=QmZaKMumAXXLkHPBV1ZdAVsF4XCUYz1Jp5PY3oEsLrKoy6&allowUpdates=true&autoActivate=true" https://netvote1.firebaseapp.com/admin/election
