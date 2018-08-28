ec2697e1f0f49c9d4726548ce1ff712462021321
Steven Landers <steven.landers@gmail.com>
2018-08-28 17:41:39 -0400

    uncache require for multi-network

--- aws/lambdas/eth-networks.js
+++ aws/lambdas/eth-networks.js
@@ -3,6 +3,7 @@ const networkMap = {}
 module.exports = {
     NetvoteProvider: async (network)=>{
         if(!networkMap[network]) {
+            delete require.cache[require.resolve('./netvote-eth.js')];
             const provider = require("./netvote-eth.js")
             await provider.Init(network);
             networkMap[network] = provider;
