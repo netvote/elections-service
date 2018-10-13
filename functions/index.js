eb2cdacf5aca7bfcdea6c508e0fc4426679ebdb1
Steven Landers <steven.landers@gmail.com>
2018-10-12 20:29:09 -0400

    add abstain validation

--- functions/index.js
+++ functions/index.js
@@ -391,7 +391,7 @@ const validateRankedChoice = (choice, metadata) => {
     const c = choice;
     const selections = c.pointsAllocations;
     if (!selections || !selections.points ) {
-        throw new Error("INVALID selections be specified for ranked type");
+        throw new Error("INVALID pointsAllocations be specified for ranked type");
     }
     if (selections.points.length !== (metadata.ballotItems.length)) {
         throw new Error("INVALID points must be allocated for each selection (or have 0 specified)");
@@ -460,9 +460,11 @@ const validateChoices = (choices, decisionsMetadata) => {
 
     choices.forEach((c, idx) => {
         let choiceType = decisionsMetadata[idx].type || "single"
-        validations[choiceType](c, decisionsMetadata[idx])
+        if(!c.abstain) {
+            validations[choiceType](c, decisionsMetadata[idx])
+        }
     });
- 
+
     return true;
 };
 
